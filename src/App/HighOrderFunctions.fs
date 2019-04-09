54f5077df8b3dce31864d59894397afd13867633
alexbispo <alexbispo.artes@gmail.com>
2019-04-09 18:07:37 -0300

    High Order Functions, Currying and Partial application.

---
+++ src/App/HighOrderFunctions.fs
@@ -0,0 +1,19 @@
+namespace ProgFuncDotNet
+
+module HighOrderFunctions =
+
+  let apply func lista  =
+    lista
+    |> List.iter func
+
+  let printNames names =
+    names
+    |> apply (printfn "Hello %s")
+
+  let printSquareOfNumbers numbers = 
+    numbers
+    |> apply (fun n -> printfn "%i." (n * 2))
+
+  let multiplyAndPrintNumbers numbers multiplicator = 
+    numbers
+    |> apply (fun n -> printfn "%i." (n * multiplicator))
---
+++ src/App/Program.fs
@@ -0,0 +1,18 @@
+﻿namespace ProgFuncDotNet
+
+module App =
+
+  [<EntryPoint>]
+  let main argv =
+    HighOrderFunctions.printNames ["Euclides"; "Sanches"; "Paulo"]
+
+    HighOrderFunctions.printSquareOfNumbers [1..10]
+
+    HighOrderFunctions.multiplyAndPrintNumbers [1..10] 3
+
+    let multiplyListFor = HighOrderFunctions.multiplyAndPrintNumbers [1..10]
+
+    multiplyListFor 4
+
+    multiplyListFor 5
+    0 // return an integer exit code
